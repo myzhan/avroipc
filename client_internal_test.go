487c466df51afcd6a91e04ad0bc8e8d14b93f2ff
slava <kulakov.home@gmail.com>
2019-12-20 01:50:09 +0300

    Add the appendBatch method #8 (#9)

--- client_internal_test.go
+++ client_internal_test.go
@@ -2,9 +2,10 @@ package avroipc
 
 import (
 	"errors"
+	"testing"
+
 	"github.com/myzhan/avroipc/mocks"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func prepare() (*client, *mocks.MockFramingLayer, *mocks.MockCallProtocol, *mocks.MockHandshakeProtocol) {
@@ -114,6 +115,68 @@ func TestClient_Append(t *testing.T) {
 		p.On("ParseResponse", method, response).Return("SOME", remaining, nil).Once()
 
 		status, err := c.Append(origEvent)
+		require.EqualError(t, err, "response buffer is not empty: len=2, rest=0x2A2B")
+		require.Equal(t, "", status)
+		p.AssertExpectations(t)
+		f.AssertExpectations(t)
+	})
+}
+
+func TestClient_AppendBatch(t *testing.T) {
+	method := "appendBatch"
+
+	request := []byte{0x0A, 0x0B}
+	response := []byte{0x1A, 0x1B}
+	remaining := []byte{0x2A, 0x2B}
+
+	origEvents := []*Event{
+		{headers: map[string]string{}, body: []byte("test body 1")},
+		{headers: map[string]string{}, body: []byte("test body 2")},
+	}
+	prepEvents := []map[string]interface{}{
+		origEvents[0].toMap(),
+		origEvents[1].toMap(),
+	}
+
+	t.Run("succeed", func(t *testing.T) {
+		c, f, p, _ := prepare()
+
+		p.On("PrepareRequest", method, prepEvents).Return(request, nil)
+		f.On("Write", request).Return(nil)
+		f.On("Read").Return(response, nil)
+		p.On("ParseResponse", method, response).Return("SOME", []byte{}, nil)
+
+		status, err := c.AppendBatch(origEvents)
+		require.NoError(t, err)
+		require.Equal(t, "SOME", status)
+		p.AssertExpectations(t)
+		f.AssertExpectations(t)
+	})
+
+	t.Run("incorrect status type", func(t *testing.T) {
+		c, f, p, _ := prepare()
+
+		p.On("PrepareRequest", method, prepEvents).Return(request, nil).Once()
+		f.On("Write", request).Return(nil).Once()
+		f.On("Read").Return(response, nil).Once()
+		p.On("ParseResponse", method, response).Return(0, []byte{}, nil).Once()
+
+		status, err := c.AppendBatch(origEvents)
+		require.EqualError(t, err, "cannot convert status to string: 0")
+		require.Equal(t, "", status)
+		p.AssertExpectations(t)
+		f.AssertExpectations(t)
+	})
+
+	t.Run("non-empty response buffer", func(t *testing.T) {
+		c, f, p, _ := prepare()
+
+		p.On("PrepareRequest", method, prepEvents).Return(request, nil).Once()
+		f.On("Write", request).Return(nil).Once()
+		f.On("Read").Return(response, nil).Once()
+		p.On("ParseResponse", method, response).Return("SOME", remaining, nil).Once()
+
+		status, err := c.AppendBatch(origEvents)
 		require.EqualError(t, err, "response buffer is not empty: len=2, rest=0x2A2B")
 		require.Equal(t, "", status)
 		p.AssertExpectations(t)
