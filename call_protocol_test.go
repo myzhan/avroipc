92f304e93986f67755b13af83d385caa7bb3750b
Vyacheslav Kulakov <vkulakov@swiftserve.com>
2019-12-28 00:27:55 +0300

    Further refactoring of tests

--- call_protocol_test.go
+++ call_protocol_test.go
@@ -9,31 +9,49 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCallProtocol_PrepareRequest(t *testing.T) {
+func prepareCallProtocol(t *testing.T) (avroipc.CallProtocol, *mocks.MockProtocol) {
 	m := &mocks.MockProtocol{}
-	m.On("PrepareMessage").Return([]byte{}, nil)
 
 	p, err := avroipc.NewCallProtocol(m)
 	require.NoError(t, err)
 
+	return p, m
+}
+
+func TestCallProtocol_PrepareRequest(t *testing.T) {
+	datum := []byte{0xA, 0xB, 0xC}
+	message := []byte{0xD, 0xE, 0xF}
+	emptyMethod := ""
+	appendMethod := "append"
+
+	nilError := error(nil)
+	testError := errors.New("test error")
+
 	t.Run("empty method", func(t *testing.T) {
-		actual, err := p.PrepareRequest("", nil)
+		p, m := prepareCallProtocol(t)
+		m.On("PrepareMessage", emptyMethod, datum).Return(message, nilError).Once()
+
+		actual, err := p.PrepareRequest(emptyMethod, datum)
 		require.NoError(t, err)
-		require.Equal(t, []byte{0x0, 0x0}, actual)
+		require.Equal(t, []byte{0x0, 0x0, 0xD, 0xE, 0xF}, actual)
+
+		m.AssertExpectations(t)
 	})
 	t.Run("append method", func(t *testing.T) {
-		actual, err := p.PrepareRequest("append", nil)
+		p, m := prepareCallProtocol(t)
+		m.On("PrepareMessage", appendMethod, datum).Return(message, nilError).Once()
+
+		actual, err := p.PrepareRequest(appendMethod, datum)
 		require.NoError(t, err)
-		require.Equal(t, []byte{0x0, 0xc, 0x61, 0x70, 0x70, 0x65, 0x6e, 0x64}, actual)
+		require.Equal(t, []byte{0x0, 0xc, 0x61, 0x70, 0x70, 0x65, 0x6e, 0x64, 0xD, 0xE, 0xF}, actual)
+
+		m.AssertExpectations(t)
 	})
 	t.Run("protocol error", func(t *testing.T) {
-		m := &mocks.MockProtocol{}
-		m.On("PrepareMessage").Return([]byte{}, errors.New("test error"))
+		p, m := prepareCallProtocol(t)
+		m.On("PrepareMessage", "append", datum).Return(message, testError).Once()
 
-		p, err := avroipc.NewCallProtocol(m)
-		require.NoError(t, err)
-
-		_, err = p.PrepareRequest("append", nil)
+		_, err := p.PrepareRequest("append", datum)
 		require.EqualError(t, err, "test error")
 
 		m.AssertExpectations(t)
@@ -41,54 +59,80 @@ func TestCallProtocol_PrepareRequest(t *testing.T) {
 }
 
 func TestCallProtocol_ParseResponse(t *testing.T) {
+	status := "test status"
+	method := "test method"
+
+	rest := []byte{}
+	data := append([]byte{0xA, 0xB, 0xC}, rest...)
+
+	longRest := []byte{0xD, 0xE, 0xF}
+	longData := append([]byte{}, longRest...)
+
+	okResponse := append([]byte{0x0, 0x0}, data...)
+	badResponse := append([]byte{0x0, 0x2}, data...)
+	longResponse := append([]byte{0x0, 0x0}, longData...)
+	shortResponse := []byte{0x0}
+	errorResponse := append([]byte{0x0, 0x1}, data...)
+	errorLongResponse := append([]byte{0x0, 0x1}, longData...)
+
+	nilError := error(nil)
+	testError := errors.New("test error")
+
 	t.Run("success", func(t *testing.T) {
-		m := &mocks.MockProtocol{}
-		m.On("ParseMessage").Return("test", []byte{}, nil)
+		p, m := prepareCallProtocol(t)
+		m.On("ParseMessage", method, data).Return(status, rest, nilError).Once()
 
-		p, err := avroipc.NewCallProtocol(m)
+		actual, err := p.ParseResponse(method, okResponse)
 		require.NoError(t, err)
-
-		actual, err := p.ParseResponse("", []byte{0x0, 0x0})
-		require.NoError(t, err)
-		require.Equal(t, "test", actual)
-
-		m.AssertExpectations(t)
-	})
-
-	t.Run("short buffer", func(t *testing.T) {
-		m := &mocks.MockProtocol{}
-
-		p, err := avroipc.NewCallProtocol(m)
-		require.NoError(t, err)
-
-		_, err = p.ParseResponse("", []byte{0x0})
-		require.EqualError(t, err, "short buffer")
+		require.Equal(t, status, actual)
 
 		m.AssertExpectations(t)
 	})
 
 	t.Run("bad flag", func(t *testing.T) {
-		m := &mocks.MockProtocol{}
+		p, m := prepareCallProtocol(t)
 
-		p, err := avroipc.NewCallProtocol(m)
-		require.NoError(t, err)
+		_, err := p.ParseResponse(method, badResponse)
+		require.EqualError(t, err, "cannot decode binary boolean: expected: Go byte(0) or byte(1); received: byte(2)")
 
-		_, err = p.ParseResponse("", []byte{0x0, 0x2})
-		require.Error(t, err)
-		require.Contains(t, err.Error(), "cannot decode binary boolean")
+		m.AssertExpectations(t)
+	})
+
+	t.Run("short buffer", func(t *testing.T) {
+		p, m := prepareCallProtocol(t)
+
+		_, err := p.ParseResponse(method, shortResponse)
+		require.EqualError(t, err, "short buffer")
 
 		m.AssertExpectations(t)
 	})
 
 	t.Run("process error", func(t *testing.T) {
-		m := &mocks.MockProtocol{}
-		m.On("ParseError").Return([]byte{}, errors.New("test error"))
+		p, m := prepareCallProtocol(t)
+		m.On("ParseError", method, data).Return(rest, testError).Once()
 
-		p, err := avroipc.NewCallProtocol(m)
-		require.NoError(t, err)
-
-		_, err = p.ParseResponse("", []byte{0x0, 0x1})
+		_, err := p.ParseResponse(method, errorResponse)
 		require.EqualError(t, err, "test error")
+
+		m.AssertExpectations(t)
+	})
+
+	t.Run("buffer not empty", func(t *testing.T) {
+		p, m := prepareCallProtocol(t)
+		m.On("ParseMessage", method, longData).Return(status, longRest, nilError).Once()
+
+		_, err := p.ParseResponse(method, longResponse)
+		require.EqualError(t, err, "response buffer is not empty: len=3, rest=0x0D0E0F")
+
+		m.AssertExpectations(t)
+	})
+
+	t.Run("process error with non-empty buffer", func(t *testing.T) {
+		p, m := prepareCallProtocol(t)
+		m.On("ParseError", method, longData).Return(longRest, nilError).Once()
+
+		_, err := p.ParseResponse(method, errorLongResponse)
+		require.EqualError(t, err, "response buffer is not empty: len=3, rest=0x0D0E0F")
 
 		m.AssertExpectations(t)
 	})
--- mocks/message_protocol.go
+++ mocks/message_protocol.go
@@ -7,16 +7,16 @@ type MockProtocol struct {
 }
 
 func (p *MockProtocol) PrepareMessage(method string, datum interface{}) ([]byte, error) {
-	args := p.Called()
+	args := p.Called(method, datum)
 	return args.Get(0).([]byte), args.Error(1)
 }
 
 func (p *MockProtocol) ParseMessage(method string, responseBytes []byte) (interface{}, []byte, error) {
-	args := p.Called()
+	args := p.Called(method, responseBytes)
 	return args.Get(0), args.Get(1).([]byte), args.Error(2)
 }
 
 func (p *MockProtocol) ParseError(method string, responseBytes []byte) ([]byte, error) {
-	args := p.Called()
+	args := p.Called(method, responseBytes)
 	return args.Get(0).([]byte), args.Error(1)
 }
